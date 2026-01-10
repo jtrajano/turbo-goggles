f1853bcbb1ae7060bd0acaeaae38fe959ee9a10d
jtrajano <jlt212000@yahoo.com>
2026-01-10 19:42:06 +0800

    create web layer

---
+++ InventoryManagement/src/InventoryManagement.WebAPI/Program.cs
@@ -0,0 +1,38 @@
+using InventoryManagement.Application;
+using InventoryManagement.Infrastructure;
+using InventoryManagement.WebAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapEndpoints();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
