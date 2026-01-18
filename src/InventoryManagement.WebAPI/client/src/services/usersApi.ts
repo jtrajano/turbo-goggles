b70dfb46732e8f0da48ceda07c7798bc317e083c
jtrajano <jlt212000@yahoo.com>
2026-01-18 17:23:02 +0800

    fix getUsers api

--- src/InventoryManagement.WebAPI/client/src/services/usersApi.ts
+++ src/InventoryManagement.WebAPI/client/src/services/usersApi.ts
@@ -1,25 +1,30 @@
+import { PagedResult } from '@/types/pageResult';
 import { User } from '@/types/user';
-import { createApi } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export const usersApi = createApi({
     reducerPath : 'usersApi',
-    baseQuery: API_BASE_URL,
+    baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL}),
     tagTypes:['Users'],
     endpoints:(build) => 
     ({
-        getUsers: build.query<{ items: User[]; totalCount: number},{search? : string; page? : number; pageSize?: number}>
+        getUsers: build.query<PagedResult<User>,{filter? : string; pageNumber? : number; pageSize?: number}>
         ({
-            query: ({ search='', page=1, pageSize= 12})=> `/users?search=${encodeURIComponent(search)}&page=${page}&pageSize=${pageSize}`,
-            providesTags: (result) => result 
-            ?[
-                ...result.items.map(
-                    ({id})=>({type: 'Users' as const, id})
-                ),
-                { type: 'Users', id: 'LIST'}
-            ]
-            :[{type: 'Users', id: 'LIST'}]
+            query: ({ filter='', pageNumber=1, pageSize= 12})=> `/users/paging?filter=${encodeURIComponent(filter)}&pageNumber=${pageNumber}&pageSize=${pageSize}`,
+            providesTags: (result) => 
+            {
+                console.log(result);
+                
+                return result 
+                ?[...result.items.map(
+                        ({id})=>({type: 'Users' as const, id})
+                    ),
+                    { type: 'Users', id: 'LIST'}
+                ]
+                :[{type: 'Users', id: 'LIST'}]
+            }
         }),
         deleteUser: build.mutation<void, string>({
             query: (id) => ({ url: `/users/${id}`, method: 'DELETE' }),
