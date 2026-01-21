4a39c2ecc199f5deb2baa0e36e3b11525dcd3c84
jtrajano <jlt212000@yahoo.com>
2026-01-21 09:11:40 +0800

    delete product command

---
+++ src/InventoryManagement.Application/Commands/DeleteProductCommand.cs
@@ -0,0 +1,29 @@
+﻿// Application/Products/Commands/DeleteProduct/DeleteProductCommand.cs
+using InventoryManagement.Application.Interfaces;
+using InventoryManagement.Domain.Exceptions;
+using MediatR;
+
+namespace InventoryManagement.Application.Products.Commands;
+
+public record DeleteProductCommand(Guid Id) : IRequest<Unit>;
+
+public class DeleteProductCommandHandler(IProductRepository _productRepository) : IRequestHandler<DeleteProductCommand, Unit>
+{
+    
+    public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (product == null)
+        {
+            throw new ProductNotFoundException(request.Id);
+        }
+
+        _productRepository.Delete(product);
+        await _productRepository.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
+
+
